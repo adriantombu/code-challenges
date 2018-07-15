70dc2633031f89d10d2a55c24706d5c2a8516999
Adrian Tombu <adrian@otso.fr>
2018-07-15 13:38:56 +0200

    Day 2 challenge

--- day-1.js
+++ day-1.js
@@ -1,60 +1,60 @@
-# https://twitter.com/AirbusCareers/status/1017047122884087808
+// https://twitter.com/AirbusCareers/status/1017047122884087808
 
 const morse = [
-	{ code: '.-', char: 'a' },
-	{ code: '-...', char: 'b' },
-	{ code: '-.-.', char: 'c' },
-	{ code: '-..', char: 'd' },
-	{ code: '.', char: 'e' },
-	{ code: '..-.', char: 'f' },
-	{ code: '--.', char: 'g' },
-	{ code: '....', char: 'h' },
-	{ code: '..', char: 'i' },
-	{ code: '.---', char: 'j' },
-	{ code: '-.-', char: 'k' },
-	{ code: '.-..', char: 'l' },
-	{ code: '--', char: 'm' },
-	{ code: '-.', char: 'n' },
-	{ code: '---', char: 'o' },
-	{ code: '.--.', char: 'p' },
-	{ code: '--.-', char: 'q' },
-	{ code: '.-.', char: 'r' },
-	{ code: '...', char: 's' },
-	{ code: '-', char: 't' },
-	{ code: '..-', char: 'u' },
-	{ code: '...-', char: 'v' },
-	{ code: '.--', char: 'w' },
-	{ code: '-..-', char: 'x' },
-	{ code: '-.--', char: 'y' },
-	{ code: '--..', char: 'z' },
-	{ code: '.-----', char: '1' },
-	{ code: '..---', char: '2' },
-	{ code: '...--', char: '3' },
-	{ code: '....-', char: '4' },
-	{ code: '.....', char: '5' },
-	{ code: '-....', char: '6' },
-	{ code: '--...', char: '7' },
-	{ code: '---..', char: '8' },
-	{ code: '----.', char: '9' },
-	{ code: '-----', char: '0' },
-	{ code: '.-.-.-', char: '.' },
-	{ code: '--..--', char: ',' },
-	{ code: '..--..', char: '?' },
-	{ code: '.----.', char: '\'' },
-	{ code: '-.-.--', char: '!' },
-	{ code: '-..-.', char: '/' },
-	{ code: '-.--.', char: '(' },
-	{ code: '-.--.-', char: ')' },
-	{ code: '.-...', char: '&' },
-	{ code: '---...', char: ':' },
-	{ code: '-.-.-.', char: ';' },
-	{ code: '-...-', char: '=' },
-	{ code: '.-.-.', char: '+' },
-	{ code: '-....-', char: '-' },
-	{ code: '..--.-', char: '_' },
-	{ code: '.-..-.', char: '"' },
-	{ code: '...-..-', char: '$' },
-	{ code: '.--.-.', char: '@' },
+  { code: '.-', char: 'a' },
+  { code: '-...', char: 'b' },
+  { code: '-.-.', char: 'c' },
+  { code: '-..', char: 'd' },
+  { code: '.', char: 'e' },
+  { code: '..-.', char: 'f' },
+  { code: '--.', char: 'g' },
+  { code: '....', char: 'h' },
+  { code: '..', char: 'i' },
+  { code: '.---', char: 'j' },
+  { code: '-.-', char: 'k' },
+  { code: '.-..', char: 'l' },
+  { code: '--', char: 'm' },
+  { code: '-.', char: 'n' },
+  { code: '---', char: 'o' },
+  { code: '.--.', char: 'p' },
+  { code: '--.-', char: 'q' },
+  { code: '.-.', char: 'r' },
+  { code: '...', char: 's' },
+  { code: '-', char: 't' },
+  { code: '..-', char: 'u' },
+  { code: '...-', char: 'v' },
+  { code: '.--', char: 'w' },
+  { code: '-..-', char: 'x' },
+  { code: '-.--', char: 'y' },
+  { code: '--..', char: 'z' },
+  { code: '.-----', char: '1' },
+  { code: '..---', char: '2' },
+  { code: '...--', char: '3' },
+  { code: '....-', char: '4' },
+  { code: '.....', char: '5' },
+  { code: '-....', char: '6' },
+  { code: '--...', char: '7' },
+  { code: '---..', char: '8' },
+  { code: '----.', char: '9' },
+  { code: '-----', char: '0' },
+  { code: '.-.-.-', char: '.' },
+  { code: '--..--', char: ',' },
+  { code: '..--..', char: '?' },
+  { code: '.----.', char: '\'' },
+  { code: '-.-.--', char: '!' },
+  { code: '-..-.', char: '/' },
+  { code: '-.--.', char: '(' },
+  { code: '-.--.-', char: ')' },
+  { code: '.-...', char: '&' },
+  { code: '---...', char: ':' },
+  { code: '-.-.-.', char: ';' },
+  { code: '-...-', char: '=' },
+  { code: '.-.-.', char: '+' },
+  { code: '-....-', char: '-' },
+  { code: '..--.-', char: '_' },
+  { code: '.-..-.', char: '"' },
+  { code: '...-..-', char: '$' },
+  { code: '.--.-.', char: '@' },
 ]
 
 const challenge =
@@ -70,13 +70,10 @@ const challenge =
 let answer = ''
 const lines = challenge.split("\n")
 
-for (line of lines) {
-	const translated = line.split(' ').map(letter => morse.filter(sign => sign.code === letter)[0].char).reverse().join('')
+for (const line of lines) {
+  const translated = line.split(' ').map(letter => morse.filter(sign => sign.code === letter)[0].char).reverse().join('')
 
-	answer = translated + answer
+  answer = translated + answer
 }
 
-console.log(answer)
-
-
-
+console.log(answer) // http://company.airbus.com/careers
---
+++ day-2.js
@@ -0,0 +1,25 @@
+// https://twitter.com/AirbusCareers/status/1017410547665403905
+
+const challenge =
+`35 31 B0 31
+36 27 34 31
+2E 38 22 4E
+20 30 B0 34
+35 27 32 34
+2E 31 22 57`
+
+let answer = ''
+const lines = challenge.split("\n")
+
+for (const line of lines) {
+  const letters = line.split(' ')
+
+  for (const letter of letters) {
+    const base10 = parseInt(letter, 16)
+    const translated = String.fromCodePoint(base10)
+
+    answer += translated
+  }
+}
+
+console.log(answer) // 51°16'41.8"N 0°45'24.1"W

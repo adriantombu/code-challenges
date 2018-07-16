70adb964909fc1d4e27ad19dce3862fadce202e8
Adrian Tombu <adrian@otso.fr>
2018-07-16 23:54:12 +0200

    Vigenere, I knew you where hidden somewhere 👀

--- day-5.js
+++ day-5.js
@@ -1,9 +1,6 @@
 // https://twitter.com/AirbusCareers/status/1018446710455103491
+const challenge = 'MIBF CL PWJTCTLM DBEW IB YBJHEV DBEW IB WMS'
 
-const challenge =
-`MIBF CL
-PWJTCTLM
-DBEW IB
-YBJHEV
-DBEW IB
-WMS`
+// https://www.dcode.fr/vigenere-cipher
+// Key: AIRBUS
+// MAKE IT POSSIBLE MAKE IT HAPPEN MAKE IT FLY

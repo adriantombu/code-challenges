2854901934ac3b49e91d17a7a33504d7ebca6ab4
Adrian Tombu <adrian@otso.fr>
2018-07-17 08:41:57 +0200

    Gotcha challenge 3 !

--- day-3.js
+++ day-3.js
@@ -1,9 +1,8 @@
 // https://twitter.com/AirbusCareers/status/1017808083173691392
+const atob = require('atob')
 
-const challenge =
-`Vm0xd1MxWnNi
-M2hpUm14VVIsa
-G9TMVZzVm1Ga
-01XdDVUVIYwV
-GxZd05IcFZSbEY
-zVUZFSw`
+const challenge = 'Vm0xd1MxWnNiM2hpUm14VVlsaG9TMVZzVm1Ga01XdDVUVlYwVGxZd05IcFZSbEYzVUZFSw'
+
+const answer = atob(atob(atob(atob(atob(challenge)))))
+
+console.log(answer) // 'We are Airbus'

7177d7000f1ae97ae8c7cd84fe3712ccab235822
Adrian Tombu <adrian@otso.fr>
2018-07-19 08:40:53 +0200

    The last one 🚀

--- day-4.js
+++ day-4.js
@@ -1,25 +1,36 @@
 // https://twitter.com/AirbusCareers/status/1018076758606786560
+const atob = require('atob')
 
-const challenge =
-`82 51 78 50 73
-69 104 119 89
-105 66 83 97 67
-66 78 98 71 99
-103 82 51 78 50
-73 69 57 121 98
-110 74 110`
+const alphabet = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+const reversedAlphabet = [ 'Z', 'Y', 'X', 'W', 'V', 'U', 'T', 'S', 'R', 'Q', 'P', 'O', 'N', 'M', 'L', 'K', 'J', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A' ]
+const challenge = `82 51 78 50 73 69 104 119 89 105 66 83 97 67 66 78 98 71 99 103 82 51 78 50 73 69 57 121 98 110 74 110`
 
 let answer = ''
-const lines = challenge.split("\n")
 
-for (const line of lines) {
-  const letters = line.split(' ')
+let step1 = ''
+const letters = challenge.split(' ')
 
-  for (const letter of letters) {
-    const translated = String.fromCodePoint(letter)
+for (const letter of letters) {
+  const translated = String.fromCodePoint(letter)
 
-    answer += translated
-  }
+  step1 += translated
 }
 
-console.log(answer) // R3N2IEhwYiBSaCBNbGcgR3N2IE9ybnJn
+const revAnswer = atob(step1) // Gsv Hpb Rh Mlg Gsv Ornrg
+
+for (let i = 0; i < revAnswer.length; i++) {
+  let result = ' '
+  const revLetter = revAnswer[i]
+
+  if (revLetter !== ' ') {
+    const isLowercase = revLetter === revLetter.toLowerCase()
+    const offset = alphabet.indexOf(revLetter.toUpperCase())
+    const decoded = reversedAlphabet[offset]
+
+    result = isLowercase ? decoded.toLowerCase() : decoded
+  }
+
+  answer += result
+}
+
+console.log(answer) // The Sky Is Not The Limit

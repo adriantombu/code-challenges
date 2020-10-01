aa3da261815d87a785ee25a0a68cee8848eb15ae
Adrian Tombu <adrian@otso.fr>
2020-10-01 23:26:54 +0200

    Optimise scrabble_score go exercism

--- exercism/go/scrabble-score/scrabble_score.go
+++ exercism/go/scrabble-score/scrabble_score.go
@@ -3,46 +3,42 @@ package scrabble
 
 import "strings"
 
+var values = map[string]int{
+	"A": 1,
+	"E": 1,
+	"I": 1,
+	"O": 1,
+	"U": 1,
+	"L": 1,
+	"N": 1,
+	"R": 1,
+	"S": 1,
+	"T": 1,
+	"D": 2,
+	"G": 2,
+	"B": 3,
+	"C": 3,
+	"M": 3,
+	"P": 3,
+	"F": 4,
+	"H": 4,
+	"V": 4,
+	"W": 4,
+	"Y": 4,
+	"K": 5,
+	"J": 8,
+	"X": 8,
+	"Q": 10,
+	"Z": 10,
+}
+
 // Score - Given a word, compute the Scrabble score for that word
 func Score(word string) int {
-	values := getValues()
-
 	var score int
+
 	for _, val := range word {
 		score += values[strings.ToUpper(string(val))]
 	}
 
 	return score
-}
-
-func getValues() map[string]int {
-	values := make(map[string]int)
-	values["A"] = 1
-	values["E"] = 1
-	values["I"] = 1
-	values["O"] = 1
-	values["U"] = 1
-	values["L"] = 1
-	values["N"] = 1
-	values["R"] = 1
-	values["S"] = 1
-	values["T"] = 1
-	values["D"] = 2
-	values["G"] = 2
-	values["B"] = 3
-	values["C"] = 3
-	values["M"] = 3
-	values["P"] = 3
-	values["F"] = 4
-	values["H"] = 4
-	values["V"] = 4
-	values["W"] = 4
-	values["Y"] = 4
-	values["K"] = 5
-	values["J"] = 8
-	values["X"] = 8
-	values["Q"] = 10
-	values["Z"] = 10
-
-	return values
 }

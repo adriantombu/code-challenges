2ee2f51dbfa6e80225c6e665feb1d276d10ed8e1
Adrian Tombu <adrian@otso.fr>
2020-11-23 14:48:15 +0100

    Exercism Luhn

--- exercism/go/luhn/luhn.go
+++ exercism/go/luhn/luhn.go
@@ -3,27 +3,28 @@
 package luhn
 
 import (
-	"errors"
-	"regexp"
 	"strconv"
 	"strings"
 )
 
 // Valid checks if the number is valid per the Luhn Formula
 func Valid(input string) bool {
-	nb, err := sanitize(input)
-	if err != nil {
+	input = strings.ReplaceAll(input, " ", "")
+	if len(input) < 2 {
 		return false
 	}
 
-	nDigits := len(nb)
-	parity := nDigits % 2
-	sum, _ := strconv.Atoi(string(nb[nDigits-1]))
+	nDigits := len(input)
+	parity := nDigits%2 == 0
+	sum := 0
 
-	for i := 0; i < nDigits-1; i++ {
-		digit, _ := strconv.Atoi(string(nb[i]))
+	for _, r := range input {
+		digit, err := strconv.Atoi(string(r))
+		if err != nil {
+			return false
+		}
 
-		if i%2 == parity {
+		if parity {
 			digit *= 2
 		}
 
@@ -31,23 +32,9 @@ func Valid(input string) bool {
 			digit -= 9
 		}
 
+		parity = !parity
 		sum += digit
 	}
 
 	return sum%10 == 0
-}
-
-func sanitize(input string) (string, error) {
-	input = strings.Replace(input, " ", "", -1)
-
-	re := regexp.MustCompile("[^0-9]+")
-	if re.Find([]byte(input)) != nil {
-		return "", errors.New("the string must only have digits")
-	}
-
-	if len(input) < 2 {
-		return "", errors.New("the string must contain at least 2 digits")
-	}
-
-	return input, nil
 }

1290a794de0c52611c3efb4585215c455afe7e1c
Adrian Tombu <adrian@otso.fr>
2020-12-01 08:50:29 +0100

    Exercism

---
+++ exercism/go/parallel-letter-frequency/parallel_letter_frequency.go
@@ -0,0 +1,35 @@
+package letter
+
+// FreqMap records the frequency of each rune in a given text.
+type FreqMap map[rune]int
+
+// Frequency counts the frequency of each rune in a given text and returns this
+// data as a FreqMap.
+func Frequency(s string) FreqMap {
+	m := FreqMap{}
+	for _, r := range s {
+		m[r]++
+	}
+	return m
+}
+
+// ConcurrentFrequency counts the frequency of letters in texts using parallel
+// computation and returns data as FreqMap
+func ConcurrentFrequency(texts []string) FreqMap {
+	c := make(chan FreqMap, 2)
+
+	for _, text := range texts {
+		go func(text string) {
+			c <- Frequency(text)
+		}(text)
+	}
+
+	m := FreqMap{}
+	for range texts {
+		for k, v := range <-c {
+			m[k] += v
+		}
+	}
+
+	return m
+}

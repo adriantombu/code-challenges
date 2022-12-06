a06f5e80ea81baa7a313458486451157e0651d08
Adrian Tombu <adrian@otso.fr>
2022-12-06 15:10:17 +0100

    Continue AOC 2022 - Day 3

--- adventofcode/2022/day-3/src/main.rs
+++ adventofcode/2022/day-3/src/main.rs
@@ -1,41 +1,53 @@
-#![feature(iter_next_chunk)]
 use std::collections::HashSet;
 
 fn main() {
     let data = include_str!("input.txt");
 
     part_one(data);
-    // part_two(data);
+    part_two(data);
 }
 
 fn part_one(data: &str) {
     let sum: u32 = data
         .lines()
-        .map(|l| {
-            let (start, end) = l.split_at(l.len() / 2);
-            let start = start.chars().map(convert_char).collect::<HashSet<_>>();
-            let end = end.chars().map(convert_char).collect::<HashSet<_>>();
+        .map(|line| {
+            let line_char = line.chars().map(convert_char).collect::<Vec<_>>();
+            let (start, end) = line_char.split_at(line.len() / 2);
+            let start = start.iter().collect::<HashSet<_>>();
+            let end = end.iter().collect::<HashSet<_>>();
 
-            start
-                .intersection(&end)
-                .collect::<Vec<_>>()
-                .pop()
-                .unwrap()
-                .to_owned()
+            **start.intersection(&end).collect::<Vec<_>>()[0]
         })
         .sum();
 
-    println!("part one intersector - total is {sum}"); // 7766
+    println!("part one - total {sum}"); // 7766
 }
 
 fn part_two(data: &str) {
-    // https://doc.rust-lang.org/stable/std/iter/trait.Iterator.html#method.next_chunk
-    let three_lines = data
-        .lines()
-        .collect::<Vec<&str>>()
-        .chunks(3)
-        .next_chunk::<3>()
-        .unwrap();
+    let lines = data.lines().collect::<Vec<&str>>();
+    let three_lines_chunks = lines.chunks(3);
+
+    let sum: u32 = three_lines_chunks
+        .map(|chunks| {
+            let sets = chunks
+                .iter()
+                .map(|chunk| chunk.chars().map(convert_char).collect::<HashSet<_>>())
+                .collect::<Vec<HashSet<_>>>();
+
+            *sets[0]
+                .intersection(&sets[1])
+                .collect::<HashSet<_>>()
+                .into_iter()
+                .copied() // Converts HashSet<&u32> to HashSet<u32>
+                .collect::<HashSet<u32>>()
+                .intersection(&sets[2])
+                .collect::<Vec<_>>()
+                .pop()
+                .unwrap()
+        })
+        .sum();
+
+    println!("part two - total {sum}"); // 2415
 }
 
 fn convert_char(c: char) -> u32 {

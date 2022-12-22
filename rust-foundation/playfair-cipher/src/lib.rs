87be3de539abd9413d05128875c1306c51eadc66
Adrian Tombu <adrian@otso.fr>
2022-12-22 15:53:06 +0100

    Add options + more tests on Playfair cipher

--- rust-foundation/playfair-cipher/src/lib.rs
+++ rust-foundation/playfair-cipher/src/lib.rs
@@ -1,16 +1,18 @@
-// TODO: remove
 // TODO: remove unwraps
-#![allow(dead_code)]
+use std::collections::HashSet;
 
 /// Implementation of the Playfair cipher
 /// More info here : https://en.wikipedia.org/wiki/Playfair_cipher
-pub struct Playfair(String);
+pub struct Playfair {
+    key: String,
+    options: Options,
+}
 
 #[allow(dead_code)]
 impl Playfair {
-    pub fn new(key: &str) -> Self {
-        // We omit the letter 'j' to have an alphabet of 25 signs (the cipher uses a 5x5 table)
-        let mut letters = "abcdefghiklmnopqrstuvwxyz".to_string();
+    pub fn new(key: &str, options: Options) -> Self {
+        let key = key.to_string().to_lowercase();
+        let mut letters = options.alphabet.clone();
 
         // Remove whitespace and duplicates from the key & alphabet
         let mut cipher = vec![];
@@ -21,6 +23,7 @@ impl Playfair {
 
             if !cipher.contains(&c) {
                 cipher.push(c);
+                // Remove from alphabet
                 letters = letters.replace(c, "");
             }
         }
@@ -28,48 +31,33 @@ impl Playfair {
         // Construct the matrix from the deduplicated key & remaining alphabet
         cipher.append(&mut letters.chars().collect::<Vec<_>>());
 
-        Playfair(String::from_iter(cipher).to_uppercase())
+        Playfair {
+            key: String::from_iter(cipher).to_uppercase(),
+            options,
+        }
     }
 
     pub fn cipher(&self, message: &str) -> String {
-        println!("cipher {}", self.0);
-        println!("----------");
-
-        let normalized_input = Self::normalize_message(message);
+        let normalized_input = self.sanitize_message(message);
         let chars = normalized_input.chars().collect::<Vec<_>>();
-        let cipher_chars = self.0.chars().collect::<Vec<_>>();
+        let cipher_chars = self.key.chars().collect::<Vec<_>>();
 
         let mut ciphered = vec![];
-        let mut i = 1;
         for chunk in chars.chunks(2) {
             let first_indice = &self
-                .0
+                .key
                 .chars()
                 .position(|c| &c == chunk.first().unwrap())
                 .unwrap();
             let last_indice = &self
-                .0
+                .key
                 .chars()
                 .position(|c| &c == chunk.last().unwrap())
                 .unwrap();
 
-            let start_row_position = first_indice % 5;
-            let end_row_position = last_indice % 5;
-            let row_position_difference = start_row_position.abs_diff(end_row_position);
-
-            println!(
-                "{i}. chunk {chunk:?} - first_indice {first_indice} - last_indice {last_indice}"
-            );
-
-            println!(
-                "start_row_position {start_row_position} - end_row_position {end_row_position} - row_position_difference {row_position_difference}"
-            );
-
             let mut new_first = 0;
             let mut new_last = 0;
             if self.is_in_row(first_indice, last_indice) {
-                println!("is in row");
-
                 new_first = first_indice + 1;
                 if new_first / 5 != first_indice / 5 {
                     new_first -= 5;
@@ -80,28 +68,20 @@ impl Playfair {
                     new_last -= 5;
                 }
             } else if self.is_in_column(first_indice, last_indice) {
-                println!("is in column");
-
                 new_first = first_indice + 5;
                 if new_first > 24 {
-                    new_first -= 24;
+                    new_first -= 25;
                 }
 
                 new_last = last_indice + 5;
                 if new_last > 24 {
-                    new_last -= 24;
+                    new_last -= 25;
                 }
             } else if self.is_in_rectangle(first_indice, last_indice) {
-                println!("is in rectangle");
-
                 // Stay in the same row but switch the positions in row between the two values
                 new_first = last_indice % 5 + (first_indice / 5 * 5);
                 new_last = first_indice % 5 + (last_indice / 5 * 5);
-            } else {
-                eprintln!("error");
             }
-
-            println!("new_first {new_first} - new_last {new_last}");
 
             let result = format!(
                 "{}{}",
@@ -109,12 +89,7 @@ impl Playfair {
                 cipher_chars.get(new_last).unwrap()
             );
 
-            println!("chunk result : {result}");
-            println!("----------");
-
             ciphered.push(result);
-
-            i += 1;
         }
 
         ciphered.join(" ")
@@ -124,7 +99,7 @@ impl Playfair {
         message.to_string()
     }
 
-    fn normalize_message(message: &str) -> String {
+    fn sanitize_message(&self, message: &str) -> String {
         // Remove punctuation, number and whitespaces
         let chars = message
             .chars()
@@ -140,8 +115,9 @@ impl Playfair {
             let last = digram.get(1).unwrap_or(&' ');
 
             normalized_input.push(first);
+            // Add the filler char if both characters are the same
             if first == last {
-                normalized_input.push(&'X')
+                normalized_input.push(&self.options.filler)
             }
 
             if !last.is_whitespace() {
@@ -151,9 +127,9 @@ impl Playfair {
 
         let mut normalized_input = String::from_iter(normalized_input);
 
-        // Add an extra 'X' to make the length even
+        // Add an extra filler (see Options.filler) to make the length even
         if normalized_input.len() % 2 != 0 {
-            normalized_input.push('X');
+            normalized_input.push(self.options.filler);
         }
 
         normalized_input
@@ -172,37 +148,151 @@ impl Playfair {
     }
 }
 
+pub struct Options {
+    alphabet: String,
+    filler: char,
+}
+
+impl Options {
+    pub fn set_filler(&mut self, filler: char) {
+        self.filler = filler;
+    }
+
+    pub fn set_alphabet(&mut self, alphabet: &str) -> Result<(), String> {
+        let letters = alphabet.chars().collect::<HashSet<_>>();
+
+        if letters.len() != 25 {
+            return Err(format!(
+                "Alphabet must be composed of 25 different letters, {} provided for {:?}",
+                letters.len(),
+                letters
+            ));
+        }
+
+        self.alphabet = alphabet.to_string();
+
+        Ok(())
+    }
+}
+
+impl Default for Options {
+    fn default() -> Self {
+        // We omit the letter 'j' to have an alphabet of 25 signs (the cipher uses a 5x5 table)
+        Options {
+            alphabet: "abcdefghiklmnopqrstuvwxyz".to_string(),
+            filler: 'X',
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_new() {
-        let pf = Playfair::new("playfair example");
+    fn test_options_set_filler() {
+        let mut options = Options::default();
+        assert_eq!(options.filler, 'X');
 
-        assert_eq!(pf.0, "PLAYFIREXMBCDGHKNOQSTUVWZ");
+        options.set_filler('Q');
+        assert_eq!(options.filler, 'Q');
     }
 
     #[test]
-    fn test_normalize_message() {
+    fn test_options_set_alphabet() {
+        let mut options = Options::default();
+        assert_eq!(options.alphabet, "abcdefghiklmnopqrstuvwxyz");
+
+        options
+            .set_alphabet("abcdefghijklmnopqrstuvxyz")
+            .expect("could not set alphabet");
+        assert_eq!(options.alphabet, "abcdefghijklmnopqrstuvxyz");
+    }
+
+    #[test]
+    fn test_options_set_alphabet_error() {
+        let mut options = Options::default();
+        assert_eq!(options.alphabet, "abcdefghiklmnopqrstuvwxyz");
+
+        let result = options.set_alphabet("abcdefghijklmnopqrstuvwxyz");
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_new() {
+        let pf = Playfair::new("playfair example", Options::default());
+
+        assert_eq!(pf.key, "PLAYFIREXMBCDGHKNOQSTUVWZ");
+    }
+
+    #[test]
+    fn test_sanitize_message() {
+        let pf = Playfair::new("playfair example", Options::default());
+
         assert_eq!(
-            Playfair::normalize_message("Hide the gold in the tree stump"),
+            pf.sanitize_message("Hide the gold in the tree stump"),
             "HIDETHEGOLDINTHETREXESTUMP"
         );
         assert_eq!(
-            Playfair::normalize_message("Cache l'or dans la souche de l'arbre"),
+            pf.sanitize_message("Cache l'or dans la souche de l'arbre"),
             "CACHELORDANSLASOUCHEDELARBRE"
         );
-        assert_eq!(Playfair::normalize_message("Hello"), "HELXLO");
-        assert_eq!(Playfair::normalize_message("Hey!"), "HEYX");
-        assert_eq!(Playfair::normalize_message("letter"), "LETXTERX");
+        assert_eq!(pf.sanitize_message("Hello"), "HELXLO");
+        assert_eq!(pf.sanitize_message("Hey!"), "HEYX");
+        assert_eq!(pf.sanitize_message("letter"), "LETXTERX");
     }
 
     #[test]
+    // Test from https://en.wikipedia.org/wiki/Playfair_cipher
     fn test_cipher_english() {
         assert_eq!(
-            Playfair::new("playfair example").cipher("hide the gold in the tree stump"),
+            Playfair::new("playfair example", Options::default())
+                .cipher("hide the gold in the tree stump"),
             "BM OD ZB XD NA BE KU DM UI XM MO UV IF"
+        );
+    }
+
+    #[test]
+    // Test from https://fr.wikipedia.org/wiki/Chiffre_de_Playfair
+    fn test_cipher_french() {
+        let mut options = Options::default();
+        options
+            .set_alphabet("abcdefghijklmnopqrstuvxyz")
+            .expect("could not set alphabet");
+
+        assert_eq!(
+            Playfair::new("exemple playfair", options)
+                .cipher("Cache l'or dans la souche de l'arbre"),
+            "BY DB XE QI BF JU ER VJ TD BL BM ER AH AL"
+        );
+    }
+
+    #[test]
+    // Test from https://it.wikipedia.org/wiki/Cifrario_Playfair
+    fn test_cipher_italian() {
+        assert_eq!(
+            Playfair::new("esempio playfair", Options::default())
+                .cipher("Le truppe sbarcheranno a Bari"),
+            "OS HG XE IS LK OC RN OH CW HA LC OC PZ"
+        );
+    }
+
+    #[test]
+    // Test from https://nl.wikipedia.org/wiki/Playfaircijfer
+    fn test_cipher_nederlands() {
+        assert_eq!(
+            Playfair::new("stalingrad", Options::default())
+                .cipher("Dit is een zeerge heimbericht."),
+            "BL AS TC CG WK FG EO KF SU GK BA EK AW"
+        );
+    }
+
+    #[test]
+    // Test from https://planetcalc.com/7751/
+    fn test_cipher_planetcalc() {
+        assert_eq!(
+            Playfair::new("Gravity Falls", Options::default()).cipher("Attack at dawn"),
+            "GF FG BM GF NF AW"
         );
     }
 }
